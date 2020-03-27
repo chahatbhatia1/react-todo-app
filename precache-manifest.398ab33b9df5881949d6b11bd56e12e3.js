6d1d34b7b1a773776f6923436fc3ae2ea7eb5439
Chahat Bhatia <chahatbhatia90@gmail.com>
2020-03-28 00:47:08 +0530

    Updates

---
+++ precache-manifest.398ab33b9df5881949d6b11bd56e12e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0d222b566fc3228155be908246f605e",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "6bad1f540229b519d079",
+    "url": "/react-todo-app/static/css/main.ac9f4d88.chunk.css"
+  },
+  {
+    "revision": "b98afb1ed8bc75bf0064",
+    "url": "/react-todo-app/static/js/2.353a51f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.353a51f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bad1f540229b519d079",
+    "url": "/react-todo-app/static/js/main.3931190a.chunk.js"
+  },
+  {
+    "revision": "985b8619f18b50d01cab",
+    "url": "/react-todo-app/static/js/runtime-main.8130e7ea.js"
+  }
+]);
